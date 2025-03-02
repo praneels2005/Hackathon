8e312223ac17c990154bfa8b5da555d2dbeb1e83
praneels2005 <praneels2005@gmail.com>
2025-03-01 20:42:07 -0500

    First Commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests as r
+
+request_url = 'https://bm-rx.atatus.com/track/browser/perf/session?apikey=995cc54b161a461aaa7eaeac727b32f5&v=4.6.4-spa'
+
+API_KEY = '995cc54b161a461aaa7eaeac727b32f5&v=4.6.4-spa'
+
+request_headers = {
+    'Request URL': request_url
+}
+
+r.post(request_url,)
+
+
+
